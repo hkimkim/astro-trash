78f6b0f71c7c35a1c5817604182c51267ba20376
Heekyung Kim <heekyung.kim27@gmail.com>
2022-02-13 11:26:36 -0500

    netlify trial 2

--- dist/ac7aca429d1bdae1dbeffbd6f7c15bbc.js
+++ dist/ac7aca429d1bdae1dbeffbd6f7c15bbc.js
@@ -68,7 +68,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({22:[function(require,module,exports) {
+})({17:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -99,7 +99,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],15:[function(require,module,exports) {
+},{}],16:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -131,7 +131,7 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":22}],0:[function(require,module,exports) {
+},{"./bundle-url":17}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
 function Module() {
@@ -149,7 +149,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://localhost:51609/');
+  var ws = new WebSocket('ws://localhost:52486/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
